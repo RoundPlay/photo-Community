c00bd9f670d445604a77151d31e18d1a1dc84277
hanyj0317 <hyj0931@naver.com>
2025-02-06 20:31:40 +0900

    feat : 로그인 화면 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import { Sidebar } from './components/sidebar';
+import Login from './pages/login';
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Sidebar></Sidebar>
+      <Routes>
+        <Route
+          path="/"
+          element={<Sidebar />}
+        />
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
