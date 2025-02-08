0fda2e94a2fd956926de3e0f986af7e400ff6dc1
hanyj0317 <hyj0931@naver.com>
2025-02-09 01:30:57 +0900

    feat: 회원가입 기능 구현 및 로그인 기능 수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,29 @@ import { Routes, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Home } from './pages/home';
-
 import Login from './pages/login';
-
 import Post from './pages/post';
+import SignUp from './pages/Signup';
 
 function App() {
   return (
     <>
       <Routes>
-
-        <Route path='/login' element={<Login />} />
-        <Route path='/post/:id' element={<Post />} />
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/login' element={<Login />} />
-
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+        <Route
+          path="/post/:id"
+          element={<Post />}
+        />
+        <Route
+          path="/"
+          element={<Home></Home>}></Route>
+        <Route
+          path="/signup"
+          element={<SignUp />}
+        />
       </Routes>
     </>
   );
